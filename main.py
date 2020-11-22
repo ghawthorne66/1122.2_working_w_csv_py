c22de059f91f17a5e900cb1af4ede069d63fa5b1
Greg Hawthorne <ghawthorne@me.com>
2020-11-22 11:07:26 +0100

    complete squirrel data

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
+
+# ==============
 # import csv
 #
 # with open("weather_data.csv") as data_file:
@@ -8,9 +27,9 @@
 #             temperatures.append(int(row[1]))
 #     print(temperatures)
 
-import pandas
-
-data = pandas.read_csv("weather_data.csv")
+# import pandas
+#
+# data = pandas.read_csv("weather_data.csv")
 
 # data_dict = data.to_dict()
 # print(data_dict)
@@ -38,29 +57,19 @@ data = pandas.read_csv("weather_data.csv")
 #
 # print(data[data.temp == data.temp.max()])
 
-monday = data[data.day == "Monday"]
-monday_temp = int(monday.temp)
-monday_temp_F = monday_temp * 9/5 + 32
-
-# print(monday.condition)
-print(monday_temp)
-print(monday_temp_F)
-
-# Create a dataframe from scratch
-data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
-}
-
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
-
-
-
-
-
-
-
-
-
-
+# monday = data[data.day == "Monday"]
+# monday_temp = int(monday.temp)
+# monday_temp_F = monday_temp * 9/5 + 32
+#
+# # print(monday.condition)
+# print(monday_temp)
+# print(monday_temp_F)
+#
+# # Create a dataframe from scratch
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+#
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
